4e89feb2db97d1cfa5141a4ab04017bda0089786
Max Adams <max@maxadams.co>
2024-01-12 01:38:53 -0800

    Name Change & Code Refactor
    - current issue: ffmpeg import conflicts

--- c.bat
+++ c.bat
@@ -1,5 +1,5 @@
 @echo off
 windres src\icon\iconadd.rc -O coff -o src\icon\iconadd.res
-gcc src\textify2.c -o Textify2 src\icon\iconadd.res
+gcc -I"C:\msys64\mingw64\include\libavutil" -I"C:\msys64\mingw64\include\libavformat" -I"C:\msys64\mingw64\include\libavcodec" src\textify.c -o Textify src\icon\iconadd.res
 pause
-start /B Textify2.exe
+start /B Textify.exe
